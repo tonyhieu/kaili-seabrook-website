1ab8c48e66074de70672b28412a59a0f3af9c5c8
Hiếu Võ <amv7668@gmail.com>
2024-06-17 20:33:22 -0700

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -12,13 +12,13 @@ function About() {
         <div className="default">
             <main className="about">
                 <div className="text-summary">
-                    <h2>Kaili Seabrook</h2>
+                    <h2>About</h2>
                     <p>Hi there! My name is Kaili and I'm an artist based in Southern California as well as a current student at Laguna College of Art and Design studying in their Game Art Program! I love making characters and doing 3D art, but aside from art you can find me studying marine biology or possibly cafe hopping :)</p>
                 </div>
 
                 <div className="text-summary">
                     <h2>Contact</h2>
-                    <p><FontAwesomeIcon icon={faEnvelope} /> &nbsp;test@gmail.com</p>
+                    <p><FontAwesomeIcon icon={faEnvelope} /> &nbsp;kaiseabrook@gmail.com</p>
                 </div>
             </main>
         </div>
